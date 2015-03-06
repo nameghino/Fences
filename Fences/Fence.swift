87fdc26b793be8783d6dabbe33ef3be17078ad46
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-03-06 15:17:08 -0300

    Add location computed property Add activate/deactivate methods Comply to MKAnnotation -title and -subtitle methods

--- Fences/Fence.swift
+++ Fences/Fence.swift
@@ -15,21 +15,48 @@ class Fence: NSObject, Equatable {
     let createdOn: NSDate = NSDate()
     let key: String = NSUUID().UUIDString
     var internalCoordinate: CLLocationCoordinate2D!
-    var range: CLLocationDistance?
+    var range: CLLocationDistance
+    var location: CLLocation {
+        get {
+            return CLLocation(latitude: internalCoordinate.latitude, longitude: internalCoordinate.longitude)
+        }
+    }
     
     override init() {
-        
+        self.range = 200
     }
     
     init(coordinate: CLLocationCoordinate2D, range: CLLocationDistance) {
         self.internalCoordinate = coordinate;
         self.range = range
     }
+    
+    func activate(locationManager: CLLocationManager) {
+        let region = CLCircularRegion(center: internalCoordinate, radius: range, identifier: key)
+        locationManager.startMonitoringForRegion(region)
+    }
+    
+    func deactivate(locationManager: CLLocationManager) {
+        for region: CLRegion in locationManager.monitoredRegions as! Set<CLRegion> {
+            if region.identifier == key {
+                locationManager.stopMonitoringForRegion(region)
+                break
+            }
+        }
+    }
 }
 
 extension Fence: MKAnnotation {
     var coordinate: CLLocationCoordinate2D {
         return internalCoordinate
+    }
+    
+    var title: String {
+        return key
+    }
+    
+    var subtitle: String {
+        return "\(createdOn)"
     }
 }
 
