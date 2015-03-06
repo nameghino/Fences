e71ee5223bfc919e3e1483adf9f0949c4dc2abce
Nico Ameghino <nicolas.ameghino@gmail.com>
2015-03-06 16:42:20 -0300

    Add compliance to NSCoding and Storeable protocols
    Add -active property

--- Fences/Fence.swift
+++ Fences/Fence.swift
@@ -11,9 +11,9 @@ import CoreLocation
 import MapKit
 
 
-class Fence: NSObject, Equatable {
-    let createdOn: NSDate = NSDate()
-    let key: String = NSUUID().UUIDString
+class Fence: NSObject, NSCoding, Equatable, Storeable {
+    var createdOn: NSDate = NSDate()
+    var key: String = NSUUID().UUIDString
     var internalCoordinate: CLLocationCoordinate2D!
     var range: CLLocationDistance
     var location: CLLocation {
@@ -22,28 +22,55 @@ class Fence: NSObject, Equatable {
         }
     }
     
+    var active: Bool
+        
     override init() {
         self.range = 200
+        self.active = false
     }
     
     init(coordinate: CLLocationCoordinate2D, range: CLLocationDistance) {
         self.internalCoordinate = coordinate;
         self.range = range
+        self.active = false
     }
     
     func activate(locationManager: CLLocationManager) {
         let region = CLCircularRegion(center: internalCoordinate, radius: range, identifier: key)
         locationManager.startMonitoringForRegion(region)
+        active = true
     }
     
     func deactivate(locationManager: CLLocationManager) {
         for region: CLRegion in locationManager.monitoredRegions as! Set<CLRegion> {
             if region.identifier == key {
                 locationManager.stopMonitoringForRegion(region)
+                active = false
                 break
             }
         }
     }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(createdOn, forKey: "createdOn")
+        aCoder.encodeObject(key, forKey: "key")
+        aCoder.encodeDouble(internalCoordinate.latitude, forKey: "internalCoordinate_latitude")
+        aCoder.encodeDouble(internalCoordinate.longitude, forKey: "internalCoordinate_longitude")
+        aCoder.encodeDouble(range, forKey: "range")
+        aCoder.encodeBool(active, forKey: "active")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        self.createdOn = aDecoder.decodeObjectForKey("createdOn") as! NSDate
+        self.key = aDecoder.decodeObjectForKey("key") as! String
+        self.internalCoordinate = CLLocationCoordinate2DMake(
+            aDecoder.decodeDoubleForKey("internalCoordinate_latitude"),
+            aDecoder.decodeDoubleForKey("internalCoordinate_longitude")
+        )
+        self.range = aDecoder.decodeDoubleForKey("range")
+        self.active = aDecoder.decodeBoolForKey("active")
+    }
+    
 }
 
 extension Fence: MKAnnotation {
@@ -64,4 +91,4 @@ func ==(lhs: Fence, rhs: Fence) -> Bool {
     return false
 }
 
-let FenceStore = Store<Fence>() { return Fence() }
+let FenceStore = Store<Fence>(filePath: filePathInDocumentsDirectory("fences.plist")) { return Fence() }
